c6ca2883785aad82e5a15a3487f8788cdbad5236
Nicolas Salaün <salaunico@yahoo.fr>
2022-05-04 12:07:53 +0200

    Ajout des squelettes des classes

---
+++ src/f128.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#define f128 __m128
+
+using namespace std;
+
+class f128
+{
+    private:
+        static int base;
+
+        static void setBase(int base) 
+        {
+            base = base;
+        }
+
+        static void print_4s(f128 reg)
+        {
+
+        }
+};
