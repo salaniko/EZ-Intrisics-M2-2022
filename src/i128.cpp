c55d9a49deae8ea764a1bc774bd00572e86eaf3d
Nicolas Salaün <salaunico@yahoo.fr>
2022-05-09 19:13:41 +0200

    Ajout affichage binaire des vecteurs SSE d'entiers

--- src/i128.cpp
+++ src/i128.cpp
@@ -10,11 +10,30 @@ using namespace std;
 
 void I128::print_16b(i128 _reg, int _base)
 {
-    uint8_t val[16];
+    int8_t val[16];
     memcpy(val, &_reg, sizeof(val));
 
     switch (_base)
     {
+        case 2:
+            int a;
+            printf("Binary: ");
+            for(int i=0; i<16; i++)
+            {
+                for (int j = 7; j >= 0; j--)
+                {
+                    a = val[i] >> j;
+
+                    if (a & 1)
+                        printf("1");
+                    else
+                        printf("0");
+                }
+                printf("  ");
+            }
+            printf("\n");
+            break;
+
         case 10:
             printf("Numerical: %i %i %i %i | %i %i %i %i | %i %i %i %i | %i %i %i %i\n",
                 val[0], val[1], val[2], val[3], val[4], val[5], val[6], val[7], 
@@ -34,11 +53,30 @@ void I128::print_16b(i128 _reg, int _base)
 
 void I128::print_8w(i128 _reg, int _base)
 {
-    uint16_t val[8];
+    int16_t val[8];
     memcpy(val, &_reg, sizeof(val));
 
     switch (_base)
-    {
+    {   
+        case 2:
+            int a;
+            printf("Binary: ");
+            for(int i=0; i<8; i++)
+            {
+                for (int j = 15; j >= 0; j--)
+                {
+                    a = val[i] >> j;
+
+                    if (a & 1)
+                        printf("1");
+                    else
+                        printf("0");
+                }
+                printf("  ");
+            }
+            printf("\n");
+            break;
+
         case 10:
             printf("Numerical: %i %i %i %i %i %i %i %i\n",
                 val[0], val[1], val[2], val[3], val[4], val[5], val[6], val[7]);
@@ -56,11 +94,30 @@ void I128::print_8w(i128 _reg, int _base)
 
 void I128::print_4d(i128 _reg, int _base)
 {
-    uint32_t val[4];
+    int32_t val[4];
     memcpy(val, &_reg, sizeof(val));
 
     switch (_base)
     {
+        case 2:
+            int a;
+            printf("Binary: ");
+            for(int i=0; i<4; i++)
+            {
+                for (int j = 31; j >= 0; j--)
+                {
+                    a = val[i] >> j;
+
+                    if (a & 1)
+                        printf("1");
+                    else
+                        printf("0");
+                }
+                printf("  ");
+            }
+            printf("\n");
+            break;
+
         case 10:
             printf("Numerical: %i %i %i %i\n",
                 val[0], val[1], val[2], val[3]);
@@ -78,18 +135,37 @@ void I128::print_4d(i128 _reg, int _base)
         
 void I128::print_2q(i128 _reg, int _base)
 {
-    uint64_t val[2];
+    int64_t val[2];
     memcpy(val, &_reg, sizeof(val));
 
     switch (_base)
     {
+        case 2:
+            int a;
+            printf("Binary: ");
+            for(int i=0; i<64; i++)
+            {
+                for (int j = 7; j >= 0; j--)
+                {
+                    a = val[i] >> j;
+
+                    if (a & 1)
+                        printf("1");
+                    else
+                        printf("0");
+                }
+                printf("  ");
+            }
+            printf("\n");
+            break;
+
         case 10:
-            printf("Numerical: %i %i\n",
+            printf("Numerical: %" PRId64 "%" PRId64 "\n",
                 val[0], val[1]);
             break;
         
         case 16:
-            printf("Hexadecimal: %#x %#x\n",
+            printf("Hexadecimal: %" PRIx64 "%" PRIx64 "\n",
                 val[0], val[1]);
             break;
         
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,31 @@
 #include "i128.hpp"
+#include "f128.hpp"
+#include "d128.hpp"
 
 
 int main(int argc, char const *argv[])
-{
-    i128 reg;
-    reg = i128_set_b(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+{   
+    // Registre avec 16 Bytes (Octets) d'entiers
+    i128 reg_b;
+    reg_b = i128_set_b(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
 
-    I128::print_16b(reg, 10);
-    I128::print_16b(reg, 16);
+    I128::print_16b(reg_b, 2);
+    I128::print_16b(reg_b, 10);
+    I128::print_16b(reg_b, 16);
+
+    // Registre avec 4 double word (32 bits) d'entiers
+    i128 reg_d1;
+    reg_d1 = i128_set_d(100, 200, 300, 400);
+
+    I128::print_4d(reg_d1, 2);
+    I128::print_4d(reg_d1, 10);
+    I128::print_4d(reg_d1, 16);
+
+    i128 reg_d2;
+    reg_d2 = i128_set_d(10, 10, 10, 10);
+
+    i128 reg_add = i128_add_d( reg_d1, reg_d2 );
+    I128::print_4d(reg_add, 10);
 
     return 0;
 }
