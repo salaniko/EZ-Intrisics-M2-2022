c08adb45392b837f1f89ecc02f4d97bae0532c41
Antoine <an.cornilleau@laposte.net>
2022-05-09 14:37:48 +0200

    Début des tests d'affichage

--- src/i128.cpp
+++ src/i128.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "i128.hpp"
 
+#ifndef __cplusplus
+#include <stdalign.h>   //C11 defines _Alignas().  This header defines alignas()
+#endif
 
 using namespace std;
-
 
 void I128::setBase(int base) 
 {
@@ -12,7 +14,26 @@ void I128::setBase(int base)
 
 void I128::print_16b(i128 reg)
 {
+    switch(base)
+    {
+        case '16':
+            printf("Registre SSE (128 bits) en HEXADECIMALE :\n"
+                    "%x %x %x %x | %x %x %x %x | %x %x %x %x | %x %x %x %x\n",
+                    reg[0], reg[1],  reg[2],  reg[3],  reg[4],  reg[5],  reg[6],  reg[7], reg[8], reg[9], reg[10], reg[11], reg[12], reg[13], reg[14], reg[15]);
 
+            break;
+        case '10':
+            printf("Registre SSE (128 bits) en DECIMAL :");
+            break;
+        case '2':
+            printf("Registre SSE (128 bits) en BINAIRE :");
+            break;
+        default:
+            printf("Registre SSE (128 bits) en HEXADECIMALE par défaut :\n"
+                   "%x %x %x %x | %x %x %x %x | %x %x %x %x | %x %x %x %x\n",
+                   reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7], reg[8], reg[9], reg[10], reg[11], reg[12], reg[13], reg[14], reg[15]);
+            break;
+    }
 }
 
 void I128::print_8w(i128 reg)
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Antoine on 06/05/2022.
+//
+#include <iostream>
+#include "i128.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv){
+    I128 i128_test = new I128;
+
+    i128_test.setBase(16);
+
+    i128 =
+
+
+    return 0;
+}
