6655880fc2fd116ac22e6300b293e353279a3a37
Nicolas Salaün <salaunico@yahoo.fr>
2022-05-11 14:41:57 +0200

    Ajout d'une première fonction pour tester l'addition de vecteurs d'entiers

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,63 @@
+#include <iostream>
+#include <stdint.h>
+#include <string.h>
+#include <inttypes.h>
+#include <math.h>
+#include <vector>
+
 #include "i128.hpp"
 #include "f128.hpp"
 #include "d128.hpp"
 
+using namespace std;
+
+
+void test_i128()
+{
+    srand48(0);
+    int32_t *a, *b, *c;
+    int32_t *pA, *pB, *pC;
+    i128 rA, rB, rC;
+
+    const int vector_size = 1000000;
+
+    a = (int32_t*) _mm_malloc (vector_size*sizeof(int32_t), 32);
+    b = (int32_t*) _mm_malloc (vector_size*sizeof(int32_t), 32);
+    c = (int32_t*) _mm_malloc (vector_size*sizeof(int32_t), 32);
+
+    for (int i=0; i<vector_size; i++)
+    {
+        a[i] = fabs(mrand48());
+        b[i] = fabs(mrand48());
+    }
+
+    pA = a; pB = b, pC = c;
+
+    for (int i=0; i<vector_size; i+=4)
+    {
+        rA = i128_lda(pA);
+        rB = i128_lda(pB);
+        rC = i128_add_d(rA, rB);
+        i128_storea(pC, rC);
+        pA += 4;
+        pB += 4;
+        pC += 4;
+    }
+}
+
+void test_f128()
+{
+
+}
+
+void test_d128()
+{
+
+}
 
 int main(int argc, char const *argv[])
 {   
-    // Registre avec 16 Bytes (Octets) d'entiers
-    i128 reg_b;
-    reg_b = i128_set_b(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
-
-    I128::print_16b(reg_b, 2);
-    I128::print_16b(reg_b, 10);
-    I128::print_16b(reg_b, 16);
-
-    // Registre avec 4 double word (32 bits) d'entiers
-    i128 reg_d1;
-    reg_d1 = i128_set_d(100, 200, 300, 400);
-
-    I128::print_4d(reg_d1, 2);
-    I128::print_4d(reg_d1, 10);
-    I128::print_4d(reg_d1, 16);
-
-    i128 reg_d2;
-    reg_d2 = i128_set_d(10, 10, 10, 10);
-
-    i128 reg_add = i128_add_d( reg_d1, reg_d2 );
-    I128::print_4d(reg_add, 10);
+    test_i128();
 
     return 0;
 }
